a962a351aca7614bbd27823f58c66a76e5232674
danpung2 <danpung_2@naver.com>
2025-06-16 01:07:40 +0900

    send -> produce

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
-import { LogPilotClient } from "logpilot-lite-client";
+import { LogPilotProducer } from "logpilot-lite-client";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const client = new LogPilotClient(process.env.LOGPILOT_SERVER_URL || "localhost:50051");
+const client = new LogPilotProducer(
+	process.env.LOGPILOT_SERVER_URL || "localhost:50051"
+);
 
 async function simulateTokenCleanup() {
-  console.log("⏱️ Job started: cleaning expired refresh tokens...");
+	console.log("⏱️ Job started: cleaning expired refresh tokens...");
 
-  const success = Math.random() > 0.5;
+	const success = Math.random() > 0.5;
 
-  const datetime = new Date();
+	const datetime = new Date();
 
-  if (!success) {
-    console.error(`[${datetime.toISOString()}] ❌ Job failed. Logging error...`);
-    await client.send({
-      channel: process.env.LOGPILOT_CHANNEL || "refresh-token-job",
-      level: "ERROR",
-      message: "Failed to clean expired refresh tokens",
-      meta: {
-        attemptId: Math.random().toString(36).substring(2),
-        reason: "Database timeout",
-      },
-      storage: "sqlite",
-    });
-  } else {
-    console.log(`[${datetime.toISOString()}] ✅ Job succeeded.`);
-  }
+	if (!success) {
+		console.error(
+			`[${datetime.toISOString()}] ❌ Job failed. Logging error...`
+		);
+		await client.produce({
+			channel: process.env.LOGPILOT_CHANNEL || "refresh-token-job",
+			level: "ERROR",
+			message: "Failed to clean expired refresh tokens",
+			meta: {
+				attemptId: Math.random().toString(36).substring(2),
+				reason: "Database timeout",
+			},
+			storage: "sqlite",
+		});
+	} else {
+		console.log(`[${datetime.toISOString()}] ✅ Job succeeded.`);
+	}
 }
 
 setInterval(simulateTokenCleanup, 5000);
